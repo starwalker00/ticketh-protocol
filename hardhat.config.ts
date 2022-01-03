bc9c78bdcbab80f29b38456386e7765fc7599c85
starwalker00 <aaronwalkerup@gmail.com>
2022-01-03 11:37:36 +0100

    network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,22 @@
 import { HardhatUserConfig } from 'hardhat/types';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
+import { node_url, accounts } from './utils/network';
 
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.1',
   },
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+  },
   namedAccounts: {
     deployer: 0,
+    buyer: 1,
   },
 };
 export default config;
